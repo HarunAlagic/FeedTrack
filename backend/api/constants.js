c81fb760af917b9393b1b36addaa1686b67267fa
Muaz Sikiric <muaaz.sikiric@gmail.com>
2024-03-31 13:56:57 +0200

    Refactor index.js

--- backend/api/constants.js
+++ backend/api/constants.js
@@ -5,6 +5,14 @@ const roles = {
   user: "user",
 };
 
+const swagger = {
+  cssUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
+  customCss:
+    ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+};
+
 module.exports = {
   roles,
+  swagger,
 };
