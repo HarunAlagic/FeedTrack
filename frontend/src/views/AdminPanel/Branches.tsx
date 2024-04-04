c923f3a7f2fe2d255bb8670d5146b65c54eeda56
HBosno <hbosno1@etf.unsa.ba>
2024-04-04 13:44:46 +0200

    Added branches and tellers table to homepage

--- frontend/src/views/AdminPanel/Branches.tsx
+++ frontend/src/views/AdminPanel/Branches.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import {useNavigate} from "react-router-dom";
-import "./branches.scss"
+import React, { useState } from 'react';
+import "./branches.scss";
+import Add from '../../components/addBranchTeller/add';
+import DataTable from '../../components/dataTableBranchTeller/dataTable';
+import { GridColDef, GridRowsProp } from '@mui/x-data-grid';
 
-const Branches = () => {
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 100 },
+    { field: 'location', headerName: 'Location', width: 150 },
+    { field: 'rating', headerName: 'Rating', width: 150 },
+    { field: 'appointee', headerName: 'Branch Appointee', width: 200 },
+  ];
+
+  const rows: GridRowsProp = [
+    { id: 1, location: 'Sarajevo', rating: '4.5', appointee: "Neko Nekic" },
+    { id: 2, location: 'Mostar', rating: '3.5', appointee: "Neko Nekic 2" },
+    { id: 3, location: 'Tuzla', rating: '4.0', appointee: "Neko Nekic 3" }
+  ];
+
+const Branches = () =>{
+    const [open, setOpen] = useState(false);
+    const [selectedRow, setSelectedRow] = useState(null);
+
+    const handleEdit = (row) => {
+        setSelectedRow(row);
+        setOpen(true);
+    };
+
+    const handleCreateNewBranch = () => {
+        setSelectedRow(null); // Reset selectedRow when creating a new teller
+        setOpen(true);
+    };
+
     return (
-        <div className="branches">Branches</div>
-    )
+        <div className="branches">
+            <div className="info">
+            <h1>Branches</h1>
+            <button onClick={handleCreateNewBranch}>Create a new branch</button>
+            </div>
+            <DataTable columns={columns} rows={rows} onEdit={handleEdit}/>
+            {open && <Add slug={selectedRow ? "edit" : "branch"} data={selectedRow} columns={columns} setOpen={setOpen} />}
+        </div>
+    );  
 }
 
-export default Branches;
+export default Branches
--- frontend/src/views/AdminPanel/Tellers.tsx
+++ frontend/src/views/AdminPanel/Tellers.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import {useNavigate} from "react-router-dom";
-import "./tellers.scss"
+import React, { useState } from 'react';
+import "./tellers.scss";
+import Add from '../../components/addBranchTeller/add';
+import DataTable from '../../components/dataTableBranchTeller/dataTable';
+import { GridColDef, GridRowsProp } from '@mui/x-data-grid';
 
-const Tellers = () => {
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 100 },
+    { field: 'branch', headerName: 'Branch ID', width: 150 },
+    { field: 'rating', headerName: 'Rating', width: 150 },
+    { field: 'appointee', headerName: 'Teller', width: 200 },
+  ];
+
+  const rows: GridRowsProp = [
+    { id: 1, branch: '1', rating: '4.5', appointee: "Neko Nekic" },
+    { id: 2, branch: '2', rating: '3.5', appointee: "Neko Nekic 2" },
+    { id: 3, branch: '3', rating: '4.0', appointee: "Neko Nekic 3" }
+  ];
+
+const Tellers = () =>{
+    const [open, setOpen] = useState(false);
+    const [selectedRow, setSelectedRow] = useState(null);
+
+    const handleEdit = (row) => {
+        setSelectedRow(row);
+        setOpen(true);
+    };
+
+    const handleCreateNewTeller = () => {
+        setSelectedRow(null); // Reset selectedRow when creating a new teller
+        setOpen(true);
+    };
+
     return (
-        <div className="tellers">Tellers</div>
-    )
+        <div className="tellers">
+            <div className="info">
+            <h1>Tellers</h1>
+            <button onClick={handleCreateNewTeller}>Create a new teller</button>
+            </div>
+            <DataTable columns={columns} rows={rows} onEdit={handleEdit}/>
+            {open && <Add slug={selectedRow ? "edit" : "teller"} data={selectedRow} columns={columns} setOpen={setOpen} />}
+        </div>
+    );  
 }
 
-export default Tellers;
+export default Tellers
